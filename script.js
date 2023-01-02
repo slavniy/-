4578bc72aa1e5dea7825a8e32479b44cbd09d150
Viktor Larionov <larionov92spb@gmail.com>
2023-01-02 16:37:36 +0300

    создали заготовку для судоку и обработку нажатия клавиши на клетку

---
+++ script.js
@@ -0,0 +1,9 @@
+let cells = document.getElementsByClassName('cell')
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', click_cell)
+  }
+
+function click_cell(event) {
+    event.target.style.backgroundColor = 'yellow';
+  }
+  
