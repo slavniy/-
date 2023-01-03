368306995a206c9b89b0553722157397a982a945
Viktor Larionov <larionov92spb@gmail.com>
2023-01-03 09:57:13 +0300

    Изменяем контент блока при нажатии

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ for (let i = 0; i < cells.length; i++) {
 
 function click_cell(event) {
     event.target.style.backgroundColor = 'yellow';
+    event.target.textContent = '+';
+    event.target.style.fontSize = '72px';
   }
   
